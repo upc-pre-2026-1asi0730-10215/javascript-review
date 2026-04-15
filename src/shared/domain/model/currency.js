d90e5f45d21989f1302cbdbaa7e7570060b9be98
李 <miauperuvian@gmail.com>
2026-04-14 20:25:05 -0500

    fix(currency): fix errors.

--- src/shared/domain/model/currency.js
+++ src/shared/domain/model/currency.js
@@ -1,32 +1,27 @@
-import {ValidationError} from "./errors";
-
-/**
- * Represents a currency value object.
- * @remarks
- * this class is used to represent a currency value with a specific code.
- */
+import { ValidationError } from "./errors";
 
 export class Currency {
-    static #VALID_CODE =['USD', 'EUR', 'GBP', 'JPY'];
-    #code
+    static #VALID_CODE = ['USD', 'EUR', 'GBP', 'JPY'];
+    #code;
 
-    /**
-     * creates a new currency instance.
-     * @throws{ValidationError} if the provided code is a valid currency code.
-     * @param code {string}
-     */
     constructor(code) {
         if (!Currency.#VALID_CODE.includes(code)) {
-                throw new ValidationError(`Invalid Currency code: ${code}. Valid codes are ${Currency.#VALID_CODE.join(', ')}.`);
-            this.#code = code;
+            throw new ValidationError(
+                `Invalid Currency code: ${code}. Valid codes are ${Currency.#VALID_CODE.join(', ')}.`
+            );
         }
 
-        get code() {
-            return this.#code;
-        }
-        equals(other){
-            return other instanceof Currency
-                && this.#code === other.code;
-        }
+        this.#code = code;
+    }
+
+    // Getter
+    get code() {
+        return this.#code;
+    }
+
+
+    equals(other) {
+        return other instanceof Currency &&
+            this.#code === other.code;
     }
 }
